d661aa3338c0da2e62388078f6d4110045fd2a95
bambang-ap <adhyt.scott@gmail.com>
2023-12-03 18:39:30 +0700

    camera

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,156 +1,22 @@
-import {MouseEventHandler, useRef, useState} from 'react';
+import Link from 'next/link';
 
-import {useForm} from 'react-hook-form';
-
-import {RouterOutput} from '@appTypes/app.type';
-import {TProduct} from '@appTypes/app.zod';
-import {FormProps} from '@appTypes/props.type';
-import {
-	Button,
-	Icon,
-	Modal,
-	ModalRef,
-	Radio,
-	Select,
-	selectMapper,
-} from '@components';
+import {Button} from '@components';
 import {getLayout} from '@layouts';
-import Scrollbar from '@prevComp/Scrollbar';
-import {numberFormat} from '@utils';
-import {trpc} from '@utils/trpc';
 
 Penjualan.getLayout = getLayout;
 
 export default function Penjualan() {
-	const modalRef = useRef<ModalRef>(null);
-	const {control, watch} = useForm();
-	const {data: categoryProducts} = trpc.category.getWithProduct.useQuery();
-	const {data: categories = []} = trpc.category.get.useQuery();
-
-	const [product, setProduct] = useState<TProduct>();
-
-	console.log(watch());
-
-	function showModal(product: TProduct) {
-		setProduct(product);
-		modalRef.current?.show();
-	}
-
 	return (
-		<div className="flex flex-1 flex-col">
-			<Modal
-				ref={modalRef}
-				onVisibleChange={v => setProduct(p => (v ? p : undefined))}>
-				<RenderProductModal control={control} product={product} />
-			</Modal>
-			<div className="flex gap-2 items-center mb-2">
-				<Select
-					control={control}
-					className="flex-1"
-					fieldName="category"
-					data={selectMapper(categories, 'id', 'name')}
-				/>
-				<Button icon="faMagnifyingGlass" />
-			</div>
-			<Scrollbar>
-				{categoryProducts?.map(product => {
-					return (
-						<RenderCategoryProduct
-							key={product.id}
-							product={product}
-							showModal={showModal}
-						/>
-					);
-				})}
-			</Scrollbar>
-		</div>
-	);
-}
+		<div className="flex flex-col gap-2">
+			<div>Ini adalah halaman utama</div>
+			<div>Untuk detail tampilan awal, bisa diskusi terlebih dahulu</div>
 
-function RenderCategoryProduct({
-	product,
-	showModal,
-}: {
-	product: RouterOutput['category']['getWithProduct'][number];
-	showModal: (product: TProduct) => void;
-}) {
-	const {name, products, icon} = product;
-
-	return (
-		<div className="p-4 my-2 rounded-lg border">
-			<div className="flex items-center gap-2">
-				{name}
-				<Icon name={icon} />
-			</div>
-
-			{products.map(product => (
-				<RenderProduct
-					onClick={() => showModal(product)}
-					key={product.id}
-					{...product}
-				/>
-			))}
-		</div>
-	);
-}
-
-function RenderProduct(
-	product: TProduct & {onClick?: MouseEventHandler<HTMLDivElement>},
-) {
-	const {name, unit, variants, onClick} = product;
-
-	const variant = variants?.[0];
-
-	return (
-		<div className="border p-2 mt-2 rounded-lg" onClick={onClick}>
-			<div>{name}</div>
 			<div>
-				{numberFormat(variant?.price ?? 0)} / 1 {unit}
+				<Link href="/registration">
+					<Button variant="outlined">Registrasi Mandiri</Button>
+				</Link>
+				<Button variant="outlined">Registrasi Mandiri</Button>
 			</div>
-		</div>
-	);
-}
-
-function RenderProductModal({
-	control,
-	product,
-}: FormProps<MyObject> & {product?: TProduct}) {
-	const {name, unit, variants = []} = product ?? {};
-	return (
-		<div>
-			<div>{name}</div>
-
-			<Radio
-				control={control}
-				fieldName="jsdf"
-				data={variants}
-				renderItem={({Radio, isFirst, isLast, item}) => {
-					const {name, price, id} = item;
-					return (
-						<div className="flex gap-2 items-center justify-between">
-							<Radio value={id} label={name} />
-							<div className="fldex-1">{numberFormat(price)}</div>
-						</div>
-					);
-				}}
-			/>
-
-			{/* {variants?.map(variant => {
-				const {id, name, price, visible} = variant;
-
-return <ButtonGroup control={control} fieldName='jsdf' key={id} data={[]}/>
-
-				return (
-					<div className="flex" key={id}>
-						<Input
-							control={control}
-							fieldName="jdf"
-							type="radio"
-							label={name}
-						/>
-					</div>
-				);
-			})} */}
 		</div>
 	);
 }
--- pages/registration.tsx
+++ pages/registration.tsx
@@ -1,7 +1,30 @@
-import {trpc} from '@utils/trpc';
+import {useRef, useState} from 'react';
+
+import Webcam from 'react-webcam';
+
+import {Button} from '@components';
+import Scrollbar from '@prevComp/Scrollbar';
 
 export default function Registration() {
-	trpc.user.get.useQuery();
+	const r = useRef<Webcam>(null);
 
-	return 'jhdfsjh';
+	const [a, b] = useState('');
+
+	return (
+		<Scrollbar>
+			<div className="flex flex-col h-full bg-green-300">
+				<Webcam ref={r} />
+				<Button
+					variant="outlined"
+					onClick={() => {
+						const u = r.current?.getScreenshot();
+						alert('sdfkjsd');
+						if (!!u) b(u);
+					}}>
+					Psadasdjk
+				</Button>
+				{!!a && <img alt="" src={a} />}
+			</div>
+		</Scrollbar>
+	);
 }
